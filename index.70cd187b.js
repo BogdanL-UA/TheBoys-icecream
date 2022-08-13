1b68c8eea7445e6ecb127a6cb445159aea43f7f4
BogdanL-UA <103260029+BogdanL-UA@users.noreply.github.com>
2022-08-13 08:38:21 +0000

    Deploying to gh-pages from @ BogdanL-UA/TheBoys-icecream@689316b78ad152ce5a5d57ab65dcc7a7228d042e 🚀

--- index.70cd187b.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,adaptiveHeight:!0,speed:500,easing:"linear",autoplay:!0,autoplaySpeed:5e3})}));
-//# sourceMappingURL=index.70cd187b.js.map
