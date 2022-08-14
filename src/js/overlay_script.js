8ba016d236b8136ec9e35edd3bfd120c68ae4ed5
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-08-14 18:56:39 +0300

    modal for products

--- src/js/overlay_script.js
+++ src/js/overlay_script.js
@@ -14,3 +14,34 @@
   });
 })();
 
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button-sec]");
+  const mobileMenuRef = document.querySelector("[data-menu-sec]");
+  
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button-third]");
+  const mobileMenuRef = document.querySelector("[data-menu-third]");
+  
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
