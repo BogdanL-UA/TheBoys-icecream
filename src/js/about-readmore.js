1e4a25944abf5ced74de2f8d341a92f2b696a730
Viktor-Panasiuk <vo151184pvv@gmail.com>
2022-08-16 17:53:33 +0300

    add animation and fix bg image

---
+++ src/js/about-readmore.js
@@ -0,0 +1,10 @@
+(() => {
+    function removeClass()
+    {
+    
+        document.getElementById('readmore-text').classList.toggle('readmore-off');
+        document.getElementById('readmore-btn-text').classList.toggle('readmore-off');
+        document.getElementById('readmore-icon').classList.toggle('readmore-off');
+    
+    }
+  })();
